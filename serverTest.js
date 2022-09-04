20543b79159ac9e7cc1226a339ae440cc3b18eff
BartyDash <writetobartek@gmail.com>
2022-09-04 14:24:58 +0200

    change package import from ESM to commonjs, chalk downgrade to version 4.1.2 + add web view + fixed register/login on the server

--- serverTest.js
+++ serverTest.js
@@ -1,11 +1,13 @@
-import mineflayer from 'mineflayer';
-import chalk from 'chalk';
-import { config } from'./config.js';
+const mineflayer = require('mineflayer');
+const { mineflayer: mineflayerViewer } = require('prismarine-viewer');
+const chalk = require('chalk');
+const { config } = require('./config.js');
 
 const bot = mineflayer.createBot({
     host: config.host,
     port: config.port,
-    username: config.username
+    username: config.username,
+    version: config.version
 });
 
 function lookAtNearestPlayer () {
@@ -35,15 +37,20 @@ bot.on('spawn', async () => {
     log(chalk.ansi256(46)(`Spawned`));
 
     bot.chatAddPattern(
-        /(register haslo haslo)/,
+        /(Jestes juz zarejestrowany|Zostales pomyslnie zarejestrowany)/,
         'register',
         'Registration required'
     );
     bot.chatAddPattern(
-        /(login haslo)/,
+        /(Jestes juz zalogowany|Zostales pomyslnie zalogowany)/,
         'loggingOn',
         'Login required'
     );
+});
+
+bot.once('spawn', () => {
+    bot.chat(`/register ${config.password} ${config.password}`);
+    mineflayerViewer(bot, { port: 3007, firstPerson: true }) // port is the minecraft server port, if first person is false, you get a bird's-eye view
 });
 
 bot.on('end', (reason) => {
@@ -56,13 +63,18 @@ bot.on('end', (reason) => {
 
 //register and login to the server
 const login = () => {
-    bot.chat(`./login ${config.password}`);
-    log(chalk.ansi256(22)('Logged on to the server')); //ansi256 22
+    log(chalk.ansi256(22)('Already logged on to the server')); //ansi256 22
+    //tutaj dać rozpoczęcie funkcji do wybierania okienek
 }
 const register = () => {
-    bot.chat(`./register ${config.password} ${config.password}`);
-    log(chalk.ansi256(22)('Registered on the server')); //ansi256 22
+    bot.chat(`/login ${config.password}`);
+    log(chalk.ansi256(22)('Already registered on the server')); //ansi256 22
 }
   
 bot.on('loggingOn', login);
 bot.on('register', register);
+
+bot.on('chat', (username, message) => {
+    if (username === bot.username) return
+    console.log(username + '  ' + message)
+  });
